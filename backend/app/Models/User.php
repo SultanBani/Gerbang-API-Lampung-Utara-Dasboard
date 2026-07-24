a7fd3bdfbf5ceaed19b9e2ac2862c578aecffcf0
Dhini Vadila <dhinivadilasari@gmail.com>
2026-07-24 08:48:17 +0700

    fix: remove unused HasApiTokens trait from User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,14 +9,13 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'username', 'email', 'password', 'role', 'opd_name', 'application_id'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * Relasi ke Application (OPD)
